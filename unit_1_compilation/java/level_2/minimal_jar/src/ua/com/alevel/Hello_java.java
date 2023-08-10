9993429cb8b0c7fd2e452180766fdfd9f32d0024
GrankinDanya <danilgrankin888@gmail.com>
2023-08-10 18:49:59 +0300

    hw_1_compilation

---
+++ unit_1_compilation/java/level_2/minimal_jar/src/ua/com/alevel/Hello_java.java
@@ -0,0 +1,23 @@
+package ua.com.alevel;
+import ua.com.alevel.test.MyMassage;
+public class Hello_java {
+    public static void main(String[] args){
+        System.out.println("Hello Java!");
+        //ua.com.alevel.test.MyMassage myMassage = new ua.com.alevel.test.MyMassage();
+        MyMassage myMassage = new MyMassage();
+        byte b = Byte.MAX_VALUE;
+        byte b1 = Byte.MIN_VALUE;
+        short s = Short.MAX_VALUE;
+        int a = Integer.MAX_VALUE;
+        long l = Long.MAX_VALUE;
+        myMassage.print(String.valueOf(b));
+        myMassage.print(String.valueOf(b1));
+        myMassage.print(String.valueOf(s));
+        myMassage.print(String.valueOf(a));
+        myMassage.print(String.valueOf(l));
+
+
+
+
+    }
+}
---
+++ unit_1_compilation/java/level_2/minimal_proj/src/ua/com/alevel/test/MyMassage.java
@@ -0,0 +1,6 @@
+package ua.com.alevel.test;
+public class MyMassage {
+    public void print(String st){
+        System.out.println(st);
+    }
+}
