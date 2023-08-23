ebe07e98928a8ed52a543b14b3b6c0e2d07e6cc0
GrankinDanya <danilgrankin888@gmail.com>
2023-08-23 21:10:05 +0300

    hw_1_compilation

---
+++ hm-2/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting the first task");
+         new task_one().taskOne();
+        System.out.println("Starting the second task");
+        new task_two().taskTwo();
+        System.out.println("Starting the third task");
+        new task_three().taskThree();
+        System.out.println("sefwe ");
+
+    }
+}
---
+++ unit_2_base_operations/src/main/java/ua/com/alevel/BaseOperations.java
@@ -0,0 +1,16 @@
+package ua.com.alevel;
+
+//import org.apache.commons.lang3.StringUtils;
+
+public class BaseOperations {
+    public static void main(String[] args) {
+//       new PrimitiveType().test();
+       new Operations().circle();
+       new Operations().ifSwitch();
+
+
+
+
+
+    }
+}
